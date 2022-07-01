6af9d6f7f3b54e2864f8b8344ed78d6e61484ee1
zacharyjulian <98063246+zacharyjulian@users.noreply.github.com>
2022-07-01 10:26:31 -0500

    Saving progress

---
+++ Assets/SwordAttack.cs
@@ -0,0 +1,48 @@
+// referenced from Chris' Tutorials 2D Game Tutorial
+// https://www.youtube.com/watch?v=7iYWpzL9GkM&t=73s
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAttack : MonoBehaviour
+{
+    public Collider2D swordCollider;
+    public float damage = 3;
+    Vector2 rightAttackOffset;
+
+    private void Start()
+    {
+        rightAttackOffset = transform.position;
+    }
+
+    public void AttackRight()
+    {
+        swordCollider.enabled = true;
+        transform.localPosition = rightAttackOffset;
+    }
+
+    public void AttackLeft()
+    {
+        swordCollider.enabled = true;
+        transform.localPosition = new Vector3(rightAttackOffset.x * -1, rightAttackOffset.y);
+    }
+
+    public void StopAttack()
+    {
+        swordCollider.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            // Deal damage to the enemy
+            //Enemy enemy = other.GetComponent<Enemy>();
+
+            //if (enemy != null)
+            //{
+                //enemy.Health -= damage;
+            //}
+        }
+    }
+}
